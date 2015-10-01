8ac2fe6e414674ae11cfb35499bf1020f6f9b439
dongja94 <dongja94@gmail.com>
2015-10-01 14:57:55 +0900

    FrameLayout

--- app/src/main/java/com/begentgroup/samplelayout/FrameActivity.java
+++ app/src/main/java/com/begentgroup/samplelayout/FrameActivity.java
@@ -1,44 +1,33 @@
 package com.begentgroup.samplelayout;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class FrameActivity extends AppCompatActivity {
+    TextView messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button btn = (Button) findViewById(R.id.btn_linear);
+        setContentView(R.layout.activity_frame);
+        messageView = (TextView)findViewById(R.id.text_message);
+        Button btn = (Button)findViewById(R.id.btn_show_message);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LinearActivity.class);
-                startActivity(intent);
+                messageView.setVisibility(View.VISIBLE);
             }
         });
 
-        btn = (Button) findViewById(R.id.btn_relative);
-        btn.setOnClickListener(new View.OnClickListener() {
-
+        messageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, RelativeActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        btn = (Button)findViewById(R.id.btn_frame);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, FrameActivity.class);
-                startActivity(intent);
+                messageView.setVisibility(View.GONE);
             }
         });
     }
@@ -46,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_frame, menu);
         return true;
     }
 
